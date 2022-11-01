aa45cad40f665cc38f09527863223da021ff1cf8
Eяw!и Gontier <ierwinx@me.com>
2022-11-01 16:32:42 -0600

    Initial Commit

---
+++ MusicPlayer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MusicPlayer
+//
+//  Created by Erwin Luz León on 01/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Erwin Luz León on 01/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
